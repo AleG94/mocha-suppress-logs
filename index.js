4f62d46cb439872ace65abeaaf91b4a24cdcd856
AleG94 <AleG94@users.noreply.github.com>
2020-06-07 16:40:46 +0200

    refactor: improve logs capturing system

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
 'use strict';
+const logCapture = require('./lib/log-capture');
 
-const levels = ['log', 'debug', 'info', 'warn', 'error'];
-const originalFunctions = levels.map(level => console[level]);
+const beforeEachCb = function () {
+  logCapture.start();
+};
 
-module.exports = function () {
-  beforeEach(function () {
-    this._logs = {};
+const afterEachCb = function () {
+  logCapture.stop();
 
-    levels.forEach(level => {
-      this._logs[level] = [];
+  if (this.currentTest.state !== 'passed') {
+    const logs = logCapture.get();
 
-      console[level] = (...args) => {
-        this._logs[level].push(args);
-      };
-    });
-  });
-
-  afterEach(function () {
-    levels.forEach((level, i) => console[level] = originalFunctions[i]);
-
-    if (this.currentTest.state !== 'passed') {
-      for (const level in this._logs) {
-        const logs = this._logs[level];
-
-        for (const args of logs) {
-          console[level](...args);
-        }
+    for (const level in logs) {
+      for (const args of logs[level]) {
+        console[level](...args);
       }
     }
-  });
+  }
+
+  logCapture.reset();
 };
+
+const suppress = function () {
+  beforeEach(beforeEachCb);
+  afterEach(afterEachCb);
+};
+
+module.exports = suppress;
+module.exports.beforeEachCb = beforeEachCb;
+module.exports.afterEachCb = afterEachCb;
